ef0b5ca488516dfe09d9828d1aee6b0151ec04b4
Dylan Hall <dylan.davidsonhall@gmail.com>
2025-11-07 20:18:18 +0000

    Updated Patient

--- Scripts/patient.py
+++ Scripts/patient.py
@@ -3,15 +3,15 @@ import requests
 url = "http://localhost:9696/predict"
 
 patient = {
-    'Age': 50,
-    'Gender': 'M',
-    'BMI': 32,
-    'Chol': 3.6,
-    'TG': 1.6,
-    'HDL': 1.4,
-    'LDL': 3.0,
-    'Cr': 72,
-    'BUN': 5
+    'Age': 49,
+    'Gender': 'F',
+    'BMI': 24,
+    'Chol': 5.29,
+    'TG': 1.74,
+    'HDL': 4.8,
+    'LDL': 4.8,
+    'Cr': 52,
+    'BUN': 4.07
 }
 response = requests.post(url, json=patient).json()
 print(response)
